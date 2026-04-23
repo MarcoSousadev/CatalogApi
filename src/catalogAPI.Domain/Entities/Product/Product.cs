62e7ec531c90ce6a557ce5a33d2babef4d6d38b0
Marco <Marco@DESKTOP-ENH6888>
2026-04-22 22:57:20 -0300

    Initial commit: Folders structure + DI config for Application and Infra + Domain Entities

---
+++ src/catalogAPI.Domain/Entities/Product/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace catalogAPI.Domain.Entities.Product
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int CategoryId { get; }
+
+        public bool IsActive { get; set; }
+
+    }
+}
---
+++ src/catalogAPI.Infrastructure/ServiceRegister.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace catalogAPI.Infrastructure
+{
+    
+        public static class ServiceRegister
+        {
+            public static void AddInfraestructure(this IServiceCollection services)
+            {
+                AddRepositories(services);
+            }
+
+            public static void AddRepositories (IServiceCollection services)
+            {
+
+            }
+        }
+ }
+
