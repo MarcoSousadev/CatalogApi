38598892a1643fad2f6bcc86ed4e32904b890035
Marco <Marco@DESKTOP-ENH6888>
2026-04-27 20:55:23 -0300

    Implementing Products repo

---
+++ src/catalogAPI.Application/Interfaces/Repositories/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using catalogAPI.Application.DTOs.Requests;
+using catalogAPI.Domain.Entities.Product;
+
+namespace catalogAPI.Application.Interfaces.Repositories
+{
+    public interface IProductRepository
+    {
+        Task CreateProduct(ProductsRequest request);
+        Task<Product> GetById(int id);
+        Task<IEnumerable<Product>> GetAllItensWithPagination(int pageNumber, int pageQuantity);
+        Task Update(int id, ProductsRequest request);
+        Task Delete(int id);
+
+    }
+}
